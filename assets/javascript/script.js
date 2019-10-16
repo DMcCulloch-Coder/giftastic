32f8f0e45078a99995d97e700e4c1deccc302971
David <DMcCulloch86@gmail.com>
2019-10-16 17:59:56 -0400

    fixed js so all gifs show on page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,8 +4,10 @@ $(document).ready(function() {
     let gifCount = 10
 
     //API query info
-    let search = "wizard"; //replace spaces with +!!!!!!!!!!
-    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=LvqdLRMeMuzOP8peDBL2nks74S42MJ0F";
+    let search = "fairy tail"; //replace spaces with +!!!!!!!!!!
+    let searchOutput = search.split(' ').join('+')
+
+    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchOutput + "&api_key=LvqdLRMeMuzOP8peDBL2nks74S42MJ0F";
     
     //calling API
     $.ajax({
@@ -15,12 +17,10 @@ $(document).ready(function() {
       console.log(response)
       for(let i = 0; i < gifCount; i++){
         console.log(response.data[i].images.original.url)
-        let imgDiv = $('.gifs').append("<div>")
-        imgDiv.html(`<img src=${response.data[i].images.original.url} />`)
+        $('.gifs').append(`<img src=${response.data[i].images.original.url} />`)
+        
       }
-      // response.data.forEach(giphy => {
-      //   console.log(giphy.url)
-      //   $('body').html("<img src=" + response.data[0].images.original.url + " />")
+        
     })
      
 })
