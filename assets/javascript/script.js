06bee59a8185201eb256b7fde6107ca600dbd547
David <DMcCulloch86@gmail.com>
2019-10-17 09:50:04 -0400

    worked on css styling, started to add Dr. Who theme and worked on button array

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,23 +4,46 @@ $(document).ready(function() {
     let gifCount = 10
 
     //API query info
-    let search = "doug";
-    let searchOutput = search.split(' ').join('+')
-
+    let search = "The Doctor";
+    let searchOutput = search.split(' ').join('+');
     let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchOutput + "&api_key=LvqdLRMeMuzOP8peDBL2nks74S42MJ0F";
+    let topicArray = ["Dr. Who", "Cybermen", "Dalek", "Tardis", "Gallifrey", "Time Lords", "The Doctor"];
     
+
+    //functions
+    function renderButtons() {
+      //clear out current gifs
+
+      $('#gifs').empty();
+
+
+    }
+
+
     //calling API
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response)
-      for(let i = 0; i < gifCount; i++){
-        console.log(response.data[i].images.original.url)
-        $('.gifs').append(`<img src=${response.data[i].images.original.url} />`)
-        
-      }
-        
-    })
-     
+    function getGifs() {
+
+      //get the new gifs
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response) //test
+        for(let i = 0; i < gifCount; i++){
+          $('.gifs').append(`<img src=${response.data[i].images.original.url} />`)
+          
+        }
+          
+      })
+    }
+
+
+  //Call functions and onclicks
+  renderButtons();
+  getGifs();
+      
 })
+
+//when doing onclick for add buttin add:
+// event.preventDefault(); // to stop it from refreshing the page
+// add conditional to not work if it is blank
